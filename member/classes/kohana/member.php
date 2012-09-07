f3570c51eda854c29f6a975455235fe5576ffe44
dishuostec <dishuostec@gmail.com>
2012-09-07 15:03:54 +0800

    move "member.php" to "kohana/member.php"

---
+++ member/classes/kohana/member.php
@@ -0,0 +1,38 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+abstract class Kohana_Member extends Kohana_Auth
+{
+	public static function instance()
+	{
+		if ( ! isset(Member::$_instance))
+		{
+			// Load the configuration for this type
+			$config = Kohana::$config->load('member');
+
+			if ( ! $type = $config->get('driver'))
+			{
+				$type = 'simple';
+			}
+
+			// Set the session class name
+			$class = 'Auth_'.ucfirst($type);
+
+			// Create a new session instance
+			Member::$_instance = new $class($config);
+		}
+
+		return Member::$_instance;
+	}
+
+	public function hash($str)
+	{
+		if ($this->_config['hash_key'])
+    {
+      return hash_hmac($this->_config['hash_method'], $str, $this->_config['hash_key']);
+    } else {
+      return $str;
+    }
+	}
+
+
+}
--- member/classes/member.php
+++ member/classes/member.php
@@ -1,38 +1,3 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
-abstract class Member extends Kohana_Auth
-{
-	public static function instance()
-	{
-		if ( ! isset(Member::$_instance))
-		{
-			// Load the configuration for this type
-			$config = Kohana::$config->load('member');
-
-			if ( ! $type = $config->get('driver'))
-			{
-				$type = 'simple';
-			}
-
-			// Set the session class name
-			$class = 'Auth_'.ucfirst($type);
-
-			// Create a new session instance
-			Member::$_instance = new $class($config);
-		}
-
-		return Member::$_instance;
-	}
-
-	public function hash($str)
-	{
-		if ($this->_config['hash_key'])
-    {
-      return hash_hmac($this->_config['hash_method'], $str, $this->_config['hash_key']);
-    } else {
-      return $str;
-    }
-	}
-
-
-}
+abstract class Member extends Kohana_Member {}
