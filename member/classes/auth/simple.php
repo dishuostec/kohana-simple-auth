bf5a71a5ac6ad8cb1e85a16ebf05c59d0cfd39a9
dishuostec <dishuostec@gmail.com>
2012-09-07 15:07:38 +0800

    load model name via config file, default is 'member'

--- member/classes/auth/simple.php
+++ member/classes/auth/simple.php
@@ -2,79 +2,90 @@
 class Auth_Simple extends Kohana_Auth_ORM
 {
 
-	public function logged_in($role = NULL)
-	{
-		$user = $this->get_user();
+  protected $_model_name = 'member';
+
+  public function __construct($config = array())
+  {
+    parent::__construct($config);
+
+    if ( ! empty($this->_config['model_name'])) {
+      $this->_model_name = $this->_config['model_name'];
+    }
+  }
+
+  public function logged_in($role = NULL)
+  {
+    $user = $this->get_user();
     return $user instanceof Model_Auth_Member AND $user->loaded();
-	}
+  }
 
-	protected function _login($user, $password, $remember)
-	{
-		if ( ! is_object($user))
-		{
-			$username = $user;
+  protected function _login($user, $password, $remember)
+  {
+    if ( ! is_object($user))
+    {
+      $username = $user;
 
-			$user = ORM::factory('copo');
-			$user->where($user->unique_key($username), '=', $username)->find();
-		}
+      $user = ORM::factory($this->_model_name);
+      $user->where($user->unique_key($username), '=', $username)->find();
+    }
 
-		if (is_string($password))
-		{
-			$password = $this->hash($password);
-		}
+    if (is_string($password))
+    {
+      $password = $this->hash($password);
+    }
 
-		if ($user->password === $password)
-		{
-			$this->complete_login($user);
+    if ($user->password === $password)
+    {
+      $this->complete_login($user);
 
-			return TRUE;
-		}
+      return TRUE;
+    }
 
-		return FALSE;
-	}
+    return FALSE;
+  }
 
-	public function force_login($user, $mark_session_as_forced = FALSE)
-	{
-		if ( ! is_object($user))
-		{
-			$username = $user;
+  public function force_login($user, $mark_session_as_forced = FALSE)
+  {
+    if ( ! is_object($user))
+    {
+      $username = $user;
 
-			// Load the user
-			$user = ORM::factory('copo');
-			$user->where($user->unique_key($username), '=', $username)->find();
-		}
+      // Load the user
+      $user = ORM::factory($this->_model_name);
+      $user->where($user->unique_key($username), '=', $username)->find();
+    }
 
-		if ($mark_session_as_forced === TRUE)
-		{
-			// Mark the session as forced, to prevent users from changing account information
-			$this->_session->set('auth_forced', TRUE);
-		}
+    if ($mark_session_as_forced === TRUE)
+    {
+      // Mark the session as forced, to prevent users from changing account information
+      $this->_session->set('auth_forced', TRUE);
+    }
 
-		// Run the standard completion
-		$this->complete_login($user);
-	}
+    // Run the standard completion
+    $this->complete_login($user);
+  }
 
-	public function get_user($default = NULL)
-	{
-		$user = parent::get_user($default);
+  public function get_user($default = NULL)
+  {
+    $user = parent::get_user($default);
 
-		return $user;
-	}
+    return $user;
+  }
 
-	public function logout($destroy = FALSE, $logout_all = FALSE)
-	{
-		// Set by force_login()
-		$this->_session->delete('auth_forced');
+  public function logout($destroy = FALSE, $logout_all = FALSE)
+  {
+    // Set by force_login()
+    $this->_session->delete('auth_forced');
 
-		return parent::logout($destroy);
-	}
+    return parent::logout($destroy);
+  }
 
- 	public function hash($str)
- 	{
- 		if ($this->_config['hash_key']) {
+  public function hash($str)
+  {
+    if ($this->_config['hash_key']) {
       return hash_hmac($this->_config['hash_method'], $str, $this->_config['hash_key']);
     } else {
       return $str;
     }
- 	}
+  }
 } // End Auth_Simple
